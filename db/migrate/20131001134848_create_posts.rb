e21c86027833a1c11b090b939d3f5eed7825682b
Devin Brown <devin.g.brown@gmail.com>
2013-10-01 07:49:38 -0600

    Blog / post scaffolding.

---
+++ db/migrate/20131001134848_create_posts.rb
@@ -0,0 +1,12 @@
+class CreatePosts < ActiveRecord::Migration
+  def change
+    create_table :posts do |t|
+      t.string :title
+      t.date :posted
+      t.string :author
+      t.text :body
+
+      t.timestamps
+    end
+  end
+end
