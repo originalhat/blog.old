5ed79a197214d37b0d9b6136bd4f70d9cef1bb62
Devin Brown <devin.g.brown@gmail.com>
2013-10-09 06:42:01 -0600

    Pagintate the FUCK out of the blog posts.

--- Gemfile
+++ Gemfile
@@ -1,5 +1,8 @@
 source 'https://rubygems.org'
 
+gem 'will_paginate'
+gem 'will_paginate-bootstrap'
+
 gem 'slim'
 
 gem 'rspec'
--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -4,7 +4,7 @@ class PostsController < ApplicationController
   # GET /posts
   # GET /posts.json
   def index
-    @posts = Post.all.reverse
+    @posts = Post.order('created_at DESC').paginate(:page => params[:page], :per_page => 5)
   end
 
   # GET /posts/1
