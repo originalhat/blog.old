90a5d14e2643ca02627185e64fdbde8abf059419
Devin Brown <devin.g.brown@gmail.com>
2013-10-17 08:13:17 -0600

    Add major feature spec for testing post-workflow.

---
+++ spec/features/post_workflow_spec.rb
@@ -0,0 +1,84 @@
+require 'spec_helper'
+
+feature "post workflow", type: :feature do
+  let(:first_title)  { "Coffee, Bacon, Business" }
+  let(:first_author) { "Mr. Winning" }
+  let(:first_body)   { "The candy man came and gave me some candy." }
+
+  let(:second_title)  { "Times are Changin'" }
+  let(:second_author) { "Mr. Losing" }
+  let(:second_body)   { "I lost my pants today." }
+
+  before(:each) do
+    visit "/"
+  end
+
+  scenario "creating, viewing, editing and deleting a post" do
+
+    # "should NOT have the user's post on the home page"
+    expect(page).to have_no_content first_title
+    expect(page).to have_no_content first_author
+    expect(page).to have_no_content first_body
+
+    # "should let the user go to the 'New Post' page"
+    within(".navbar") do
+      click_link "New Post"
+    end
+
+    # "should be on the 'New Post' page"
+    expect(page).to have_content "A Fresh Brew..."
+    expect(page).to have_content "Title"
+    expect(page).to have_content "Author"
+    expect(page).to have_content "Body"
+
+    # "should let the user create a new post"
+    fill_in "post_title",  with: first_title
+    fill_in "post_author", with: first_author
+    fill_in "post_body",   with: first_body
+    click_button "Create Post"
+
+    # "should have the user's post on the home page"
+    expect(page).to have_content first_title
+    expect(page).to have_content first_author
+    expect(page).to have_content first_body
+
+    # "should let the user go to the home page"
+    click_link "The Grndz Blog"
+
+    # "should let the user go to the post's page"
+    click_link first_title
+
+    # "should have the user's post on the post's page"
+    expect(page).to have_content first_title
+    expect(page).to have_content first_author
+    expect(page).to have_content first_body
+
+    # "should let the user go to edit the post page"
+    click_link "Edit"
+
+    # "should be on the edit post's page"
+    expect(page).to have_content "Editing Post"
+    expect(page).to have_content "Title"
+    expect(page).to have_content "Author"
+    expect(page).to have_content "Body"
+
+    # "should let the user edit the post"
+    fill_in "post_title",  with: second_title
+    fill_in "post_author", with: second_author
+    fill_in "post_body",   with: second_body
+    click_button "Update Post"
+
+    # "should have the user's post on the post's page"
+    expect(page).to have_content second_title
+    expect(page).to have_content second_author
+    expect(page).to have_content second_body
+
+    # "should let the user delete their post"
+    click_link "Delete"
+
+    # "should NOT have the user's post on the home page"
+    expect(page).to have_no_content second_title
+    expect(page).to have_no_content second_author
+    expect(page).to have_no_content second_body
+  end
+end
