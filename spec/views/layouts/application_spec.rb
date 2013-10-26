ace83ecdc72b045207c0d4d63ec08a5c462468b0
Devin Brown <devin.g.brown@gmail.com>
2013-10-25 22:22:05 -0600

    Add sexy title.

--- spec/views/layouts/application_spec.rb
+++ spec/views/layouts/application_spec.rb
@@ -5,6 +5,10 @@ describe "layouts/application" do
     render
   end
 
+  it "should have the correct page title" do
+    response.should have_content "Grndz - The Ultimate Coffee Destination"
+  end
+
   it "should render the google analytics partial" do
     response.should render_template "shared/_google_analytics"
   end
